c41e01cf2dcd24271c98aeaaff91024762459b9f
AngryCouchPotato <alexeykolcov@gmail.com>
2019-10-13 23:31:31 +0200

    -add component for Google Map(raw version)

--- src/Constants.js
+++ src/Constants.js
@@ -1,1 +1,7 @@
 export const SERVER_URL = 'http://localhost:9090/'
+export const GOOGLE_MAP_API_KEY = 'AIzaSyD6ieBKMSPKT3Nh4mIPwuHqR8MhbA7PgH0'
+export const MAP_STYLES = {
+    width: '100%',
+    height: '100%',
+};
+
