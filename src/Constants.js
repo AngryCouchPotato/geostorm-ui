3245da70e93f34a8a2761e2c25c07de5cffe00ba
AngryCouchPotato <alexeykolcov@gmail.com>
2019-10-16 13:58:55 +0200

    -Heroku Deployment

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,6 @@
-export const SERVER_URL = 'http://localhost:9090/'
-export const GOOGLE_MAP_API_KEY = 'GOOGLE_MAP_API_KEY'
+//export const SERVER_URL = 'http://localhost:9090/'
+export const SERVER_URL = 'https://geo-storm-backend.herokuapp.com/'
+export const GOOGLE_MAP_API_KEY = 'AIzaSyD6ieBKMSPKT3Nh4mIPwuHqR8MhbA7PgH0'
 export const MAP_STYLES = {
     width: '100%',
     height: '100%',
